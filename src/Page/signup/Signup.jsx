4fe9df8811860c98691972c153634fc415d7e3b6
MyNameIsYGKim <kelen536@gmail.com>
2025-01-02 16:31:01 +0900

    250102 김연규(오류 수정)

--- src/Page/signup/Signup.jsx
+++ src/Page/signup/Signup.jsx
@@ -1,10 +1,15 @@
-
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { SignupContainer } from "../../Component/SignupComponent";
 
 const Signup = () => {
 
 
   return(
-    
+    <SignupContainer>
+      
+    </SignupContainer>
   );
 };
 export default Signup;
