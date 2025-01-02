641ad06cfb5abbd7e73e4c238b4660fac9e7ad65
MyNameIsYGKim <kelen536@gmail.com>
2025-01-02 16:29:59 +0900

    250102 김연규(App.js 복구)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Planning } from "./Page/Planning/Planning";
 import { Main } from "./Page/MainPage";
-import { Login } from "./Page/signup/Login"
-import { Signup } from "./Page/signup/Signup"
 
 function App() {
   return (
@@ -11,8 +9,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/planning" element={<Planning />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
         </Routes>
       </Router>
     </>
