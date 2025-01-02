53ce680208ab326de01ac430e4a45aa26db88bb1
MyNameIsYGKim <kelen536@gmail.com>
2025-01-02 16:24:47 +0900

    250102 김연규(App.js 추가)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Planning } from "./Page/Planning/Planning";
 import { Main } from "./Page/MainPage";
+import { Login } from "./Page/signup/Login"
+import { Signup } from "./Page/signup/Signup"
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/planning" element={<Planning />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </Router>
     </>
