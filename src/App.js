33873cb9e3df6bdab8ff03ad94aa65703b256136
MyNameIsYGKim <kelen536@gmail.com>
2025-01-07 17:08:22 +0900

    김연규( App.js 수정)

--- src/App.js
+++ src/App.js
@@ -9,28 +9,31 @@ import { Signup } from "./Page/signup/Signup";
 import MyPageMain from "./Page/Mypage/MyPageMain";
 import KtxInquiry from "./Page/Traffic/KtxInquiry";
 import { TourList } from "./Page/Item/TourList";
-import { PlanningList } from "./Page/Item/PlanningList";
+import { PlanningList } from "./Page/Item/PlanningList";\
+import { AuthProvider } from "./Context/AuthContext";
 
 function App() {
   return (
     <>
-      <DndProvider backend={HTML5Backend}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/planning" element={<Planning />} />
-            <Route path="/makePlanning" element={<MakePlanning />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/mypage" element={<MyPageMain />} />
-            <Route path="/ktxInquiry" element={<KtxInquiry />} />
-            <Route path="/tourlist" element={<TourList />} />
-            <Route path="/tourlist/:areaCode" element={<TourList />} />
-            <Route path="/planninglist" element={<PlanningList />} />
-            <Route path="/planninglist/:areaCode" element={<PlanningList />} />
-          </Routes>
-        </Router>
-      </DndProvider>
+      <AuthProvider>
+        <DndProvider backend={HTML5Backend}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/planning" element={<Planning />} />
+              <Route path="/makePlanning" element={<MakePlanning />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/mypage" element={<MyPageMain />} />
+              <Route path="/ktxInquiry" element={<KtxInquiry />} />
+              <Route path="/tourlist" element={<TourList />} />
+              <Route path="/tourlist/:areaCode" element={<TourList />} />
+              <Route path="/planninglist" element={<PlanningList />} />
+              <Route path="/planninglist/:areaCode" element={<PlanningList />} />
+            </Routes>
+          </Router>
+        </DndProvider>
+      </AuthProvider>
     </>
   );
 }
