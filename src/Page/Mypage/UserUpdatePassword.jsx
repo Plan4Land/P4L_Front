bad088a7bcc7c571052ba1c09828c75a974c5722
MyNameIsYGKim <131988865+MyNameIsYGKim@users.noreply.github.com>
2025-01-08 23:09:36 +0900

    250108 김연규

--- src/Page/Mypage/UserUpdatePassword.jsx
+++ src/Page/Mypage/UserUpdatePassword.jsx
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { Center, Container, InputBox, Button } from "../../Style/UserInfoEditStyle";
+import { useRef } from "react";
+import {
+  Center,
+  Container,
+  InputBox,
+  Button,
+} from "../../Style/UserInfoEditStyle";
+import AxiosApi from "../../Api/AxiosApi";
 
 const UserUpdatePassword = () => {
   const [userPw, setUserPw] = useState("");
+  const userPwRef = useRef();
   const [userPwCheck, setUserPwCheck] = useState("");
+  const userPwCheckRef = useRef();
   const [message, setMessage] = useState("");
 
   const [isPwSame, setIsPwSame] = useState("");
-
 
   // 비밀번호 같은지 확인
   const handlePwCheck = (e) => {
@@ -16,24 +24,30 @@ const UserUpdatePassword = () => {
   };
 
   const handleSubmit = async () => {
-    if(!userPw.trim()) {
+    if (!userPw.trim()) {
       setMessage("비밀번호를 입력하세요.");
+      userPwRef.current.focus();
       return;
     }
-    if(userPw.length < 8) {
+    if (userPw.length < 8) {
       setMessage("비밀번호는 8자 이상이어야 합니다.");
       return;
     }
-    if(!/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_]).{8,}$/.test(userPw)) {
+    if (!/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_]).{8,}$/.test(userPw)) {
       setMessage("비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.");
       return;
     }
-    if(userPw !== userPwCheck) {
+    if (userPw !== userPwCheck) {
       setMessage("비밀번호가 일치하지 않습니다.");
       return;
     }
 
     // 기능 구현
+    try {
+    } catch (error) {
+      console.error("Error during update password: ", error);
+      setMessage("비밀번호 변경 도중 오류가 발생했습니다.");
+    }
   };
 
   return (
@@ -48,6 +62,7 @@ const UserUpdatePassword = () => {
         <InputBox>
           <div className="inputBox">
             <input
+              ref={userPwRef}
               id="userPw"
               type="password"
               value={userPw}
@@ -65,6 +80,7 @@ const UserUpdatePassword = () => {
         <InputBox>
           <div className="inputBox">
             <input
+              ref={userPwCheckRef}
               id="userPwCheck"
               type="password"
               value={userPwCheck}
@@ -73,9 +89,7 @@ const UserUpdatePassword = () => {
           </div>
         </InputBox>
 
-        <Button onClick={handleSubmit}>
-          변경
-        </Button>
+        <Button onClick={handleSubmit}>변경</Button>
       </Container>
     </Center>
   );
