796f6b16a5b0341f41d0e878bcffe36ef586b7cb
MyNameIsYGKim <kelen536@gmail.com>
2025-01-13 14:17:20 +0900

    250113 김연규(회원정보수정 - 사진 업로드 구현)

--- src/Page/Mypage/UserInfoEdit.jsx
+++ src/Page/Mypage/UserInfoEdit.jsx
@@ -3,6 +3,7 @@ import { Center, Container, InputBox, Button } from "../../Style/UserInfoEditSty
 import { ProfilePicModal } from "../../Component/SignupModalComponent";
 import { useAuth } from "../../Context/AuthContext";
 import AxiosApi from "../../Api/AxiosApi";
+import { storage } from "../../Api/Firebase";
 
 const UserInfoEdit = () => {
   const { user } = useAuth();
@@ -22,7 +23,21 @@ const UserInfoEdit = () => {
   const handlePicAdd = (picture) => {
     // 사진 추가하는 기능 구현하기.
     setCurrentPic(URL.createObjectURL(picture));
-    console.log(picture);
+
+    const storageRef = storage.ref(`/UserProfilePic/${user.id}/`); // Firebase Storage 참조
+    const fileRef = storageRef.child(picture.name); // 파일 저장 위치 지정
+    fileRef.put(picture)  // 파일 업로드
+      .then(() => {
+        console.log("파일 업로드 성공.");
+        return fileRef.getDownloadURL(); // 업로드된 파일의 URL 가져오기
+      })
+      .then((downloadUrl) => {
+        console.log("저장된 경로:", downloadUrl);
+        setCurrentPic(downloadUrl); // 이미지 URL 상태 업데이트
+      })
+      .catch((error) => {
+        console.error("업로드 중 에러 발생:", error);
+      });
   };
 
   // 정보 가져오기
