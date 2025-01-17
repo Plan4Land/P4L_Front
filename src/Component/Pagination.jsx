464b389a9ad05ab829b64a083734723f67cda9d7
BeigeSeason <kima606@gmail.com>
2025-01-17 09:30:29 +0900

    2025.01.16 강휘 페이지 5개 고정

--- src/Component/Pagination.jsx
+++ src/Component/Pagination.jsx
@@ -1,46 +1,46 @@
 import React from "react";
 import styled from "styled-components";
-import { colors } from "../Style/GlobalStyle";
+import {colors} from "../Style/GlobalStyle";
 
 const PaginationContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
 
-  button {
-    padding: 8px 16px;
-    margin: 0 5px;
-    font-size: 14px;
-    border: none;
-    background-color: transparent;
-    cursor: pointer;
-    transition: background-color 0.3s, border-color 0.3s;
-  }
+    button {
+        padding: 8px 16px;
+        margin: 0 5px;
+        font-size: 14px;
+        border: none;
+        background-color: transparent;
+        cursor: pointer;
+        transition: background-color 0.3s, border-color 0.3s;
+    }
 
-  button:hover {
-    font-weight: bold;
-    color: ${colors.colorA};
-  }
+    button:hover {
+        font-weight: bold;
+        color: ${colors.colorA};
+    }
 
-  button.active {
-    font-weight: bold;
-    text-decoration: underline;
-    color: ${colors.colorA};
-  }
+    button.active {
+        font-weight: bold;
+        text-decoration: underline;
+        color: ${colors.colorA};
+    }
 
-  button:disabled {
-    background-color: #e0e0e0;
-    color: #999;
-    cursor: not-allowed;
-  }
+    button:disabled {
+        background-color: #e0e0e0;
+        color: #999;
+        cursor: not-allowed;
+    }
 `;
 
 // 페이지네이션 컴포넌트
-export const Pagination = ({ currentPage, totalPages, handlePageChange }) => {
+export const Pagination = ({currentPage, totalPages, handlePageChange}) => {
   const pageNumbers = [];
 
-  const firstPage = Math.max(currentPage - 2, 1);
+  const firstPage = currentPage + 1;
   const lastPage = Math.min(currentPage + 5, totalPages);
 
   for (let i = firstPage; i <= lastPage; i++) {
