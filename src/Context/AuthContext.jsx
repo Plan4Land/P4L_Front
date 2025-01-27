1c59d1da367fe1acd1032170162df02d84e77d49
BeigeSeason <kima606@gmail.com>
2025-01-27 15:21:05 +0900

    2025.01.27 강휘 어드민 페이지 진입 시 자동 로그아웃 처리

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -17,7 +17,7 @@ export const AuthProvider = ({ children }) => {
     if (user) {
       localStorage.setItem("user", JSON.stringify(user));
     } else {
-      localStorage.removeItem("user");
+      localStorage.clear();
     }
   }, [user]);
 
