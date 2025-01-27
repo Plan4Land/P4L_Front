74c94452f8c7cb7f9f05763bc19a0eb59158f986
hmya_hnya <kh120205@gmail.com>
2025-01-27 16:31:26 +0900

    250127kh-planning

--- src/Component/GlobalComponent.jsx
+++ src/Component/GlobalComponent.jsx
@@ -7,7 +7,7 @@ import AxiosApi from "../Api/AxiosApi";
 // import { TopTourApi, TopPlanApi } from "../Api/ItemApi";
 import { areas } from "../Util/Common";
 import { MyPlannerApi, BookmarkedSpotsApi } from "../Api/ItemApi";
-import Logo from "../Img/Plan4landLogo.png";
+import Logo from "../Img/plan4landlogo.png";
 import Title from "../../src/Img/plan4land_.png";
 import { Outlet } from "react-router-dom";
 
