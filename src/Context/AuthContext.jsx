6804e6d15a41a83b4e2cc43823b8c4f403a54cba
MyNameIsYGKim <131988865+MyNameIsYGKim@users.noreply.github.com>
2025-02-14 20:46:52 +0900

    250214 김연규(로그아웃 소셜 토큰 삭제)

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -2,6 +2,8 @@ import react, { createContext, useState, useContext, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Common from "../Util/Common";
 import { refreshToken } from "../Util/RefreshToken";
+import axios from "axios";
+import AxiosApi from "../Api/AxiosApi";
 
 const AuthContext = createContext();
 
@@ -15,7 +17,7 @@ export const AuthProvider = ({ children }) => {
   const [socialToken, setSocialToken] = useState(() => {
     const storedSocialToken = localStorage.getItem("socialToken");
     return storedSocialToken ? JSON.parse(storedSocialToken) : null;
-  })
+  });
 
   useEffect(() => {
     if (user) {
@@ -30,9 +32,54 @@ export const AuthProvider = ({ children }) => {
     const { id, nickname, imgPath, role } = userData.data;
     setUser({ id, nickname, imgPath, role });
   };
-  const logout = () => {
+  const logout = async () => {
+    // 로컬 비우기
     setUser(null);
-    
+    // 소셜 연결 끊기
+    const userInfo = await AxiosApi.memberInfo(user.id);
+    if (userInfo.data.sso === "kakao") {
+      axios
+        .get(`https://kapi.kakao.com/v1/user/unlink`, {
+          headers: {
+            Authorization: `Bearer ${socialToken.accessToken}`,
+          },
+          params: {
+            target_id_type: "user_id",
+            target_id: userInfo.data.socialId,
+          },
+        })
+        .then((response) => {
+          console.log("카카오 계정 연결 끊기 성공", response);
+        })
+        .catch((error) => {
+          console.log("카카오 계정 연결 끊기 실패", error);
+        });
+    } else if (userInfo.data.sso === "google") {
+      const accessToken = socialToken.accessToken;
+      axios
+        .get(`https://oauth2.googleapis.com/revoke?token=${accessToken}`)
+        .then((response) => {
+          console.log("구글 계정 연결 끊기 성공", response);
+        })
+        .catch((error) => {
+          console.log("구글 계정 연결 끊기 실패", error);
+        });
+    } else if (userInfo.data.sso === "naver") {
+      const accessToken = socialToken.accessToken;
+      axios
+        .get("https://nid.naver.com/oauth2.0/token", {
+          params: {
+            access_token: accessToken,
+            grant_type: "delete",
+          },
+        })
+        .then((response) => {
+          console.log("네이버 계정 연결 끊기 성공", response);
+        })
+        .catch((error) => {
+          console.log("네이버 계정 연결 끊기 실패", error);
+        });
+    }
   };
   const socialLogin = (token) => {
     const { accessToken, refreshToken } = token.data;
@@ -66,7 +113,7 @@ export const AuthProvider = ({ children }) => {
 
   //       if (isValidating) return;
   //       setIsValidating(true);
-  
+
   //       const valid = isAccessTokenValid();
   //       try {
   //         if (!valid) {
@@ -82,13 +129,12 @@ export const AuthProvider = ({ children }) => {
   //         setIsValidating(false);
   //       }
   //     };
-  
+
   //     validateToken();
   //   }, [location]);
 
   //   return null;
   // };
-  
 
   // 액세스 토큰 만료기간이 안 됐으면 true, 없거나 지났으면 false
   const isAccessTokenValid = () => {
